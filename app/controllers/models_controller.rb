db9774bdfb28ff625fc758073d54b8a160609d29
JohnnySpomer <jonispom@gmail.com>
2018-10-25 16:23:49 -0500

    finished project with 100%

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,5 +1,5 @@
 class ModelsController < ApplicationController
   def show
-    
+    @model = Model.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get 'locations/view'
   get 'about/show'
   get 'vehicle_types/show'
-  get 'models/show'
+  get 'models/:id', to: "models#show", as: 'model'
   get 'manufacturers', to: "manufacturers#index", as: 'manufacturers'
   get 'manufacturers/:id', to: "manufacturers#show", as: 'manufacturer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
